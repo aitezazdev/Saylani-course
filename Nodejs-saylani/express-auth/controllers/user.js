010ae4786b290da9b1d06e9708d6beadd00fd9a2
aitezazdev <aitezazsikandar@gmail.com>
2025-03-02 11:11:54 +0500

    add node-mailer

--- Nodejs-saylani/express-auth/controllers/user.js
+++ Nodejs-saylani/express-auth/controllers/user.js
@@ -1,11 +1,48 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "aitezazsikandar@gmail.com",
+    pass: process.env.APP_GOOGLE_PASSWORD,
+  },
+});
+
+const mailOptions = {
+  from: "aitezazsikandar@gmail.com",
+  to: "aitezazsikandar@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
 
 const userData = {
   id: 123,
   username: "Zaz",
   password: "password123",
   role: "user",
+};
+
+const sendEmail = (req, res) => {
+  try {
+    transport.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const register = async (req, res) => {
@@ -58,4 +95,4 @@ const login = async (req, res) => {
 };
 
 
-module.exports = { register, login };
+module.exports = { register, login, sendEmail };
--- Nodejs-saylani/express-auth/routes/user.js
+++ Nodejs-saylani/express-auth/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/user.js");
+const { register, login, sendEmail } = require("../controllers/user.js");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/send-email", sendEmail);
 
 module.exports = router;
